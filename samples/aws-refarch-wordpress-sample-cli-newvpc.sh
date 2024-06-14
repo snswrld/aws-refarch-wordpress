3862bb83b163dbafbbebb16e6e9ea9218e41a7bc
senseworld <d@senseworldcafe.com>
2024-06-13 18:10:44 -0700

    Updated locations to redirect to this repo

--- samples/aws-refarch-wordpress-sample-cli-newvpc.sh
+++ samples/aws-refarch-wordpress-sample-cli-newvpc.sh
@@ -1,6 +1,6 @@
 aws cloudformation create-stack \
  --stack-name WordPress \
- --template-body https://s3.amazonaws.com/aws-refarch/wordpress/latest/templates/aws-refarch-wordpress-master-newvpc.yaml \
+ --template-body https://github.com/snswrld/aws-refarch-wordpress/tree/master/templates/aws-refarch-wordpress-master-newvpc.yaml \
  --parameters file://aws-refarch-wordpress-parameters-newvpc.json \
  --capabilities CAPABILITY_IAM \
  --disable-rollback \
